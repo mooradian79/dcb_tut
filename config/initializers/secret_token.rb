c1d6664d34534ed2b55166f78b41e5ab3b43329a
mooradian79 <martin_mooradian@yahoo.com>
2012-05-20 10:19:52 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DcbTut::Application.config.secret_token = 'd58db7b94601c2c75ea8c6ce64837ced6d9ad64868134567db76f771299d14e8b16a3341f31744cfe4fe184f56943d4e58a3ddd9dbb259db1e9d9c2f5715a78b'
